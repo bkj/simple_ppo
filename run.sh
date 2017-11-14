c46b6cc79549e569bece758c67073960b0c5e5c1
bjohnson <bkj.322@gmail.com>
2017-11-14 17:05:03 -0500

    typo

--- run.sh
+++ run.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-mkdir -p results/refactor
-
-python mlp-main.py --total-steps 1000000 --env Reacher-v1 > ./results/refactor/Reacher-constant-no_rms
-python mlp-main.py --total-steps 1000000 --env Reacher-v1 --rms > ./results/refactor/Reacher-constant-rms
-
-python mlp-main.py --total-steps 1000000 --env Hopper-v1 > ./results/refactor/Hopper-constant-no_rms
-python mlp-main.py --total-steps 1000000 --env Hopper-v1 --rms > ./results/refactor/Hopper-constant-rms
-
-python mlp-main.py --total-steps 1000000 --env HalfCheetah-v1 > ./results/refactor/HalfCheetah-constant-no_rms
-python mlp-main.py --total-steps 1000000 --env HalfCheetah-v1 --rms > ./results/refactor/HalfCheetah-constant-rms
-
-python mlp-main.py --total-steps 1000000 --env InvertedPendulum-v1 > ./results/refactor/InvertedPendulum-constant-no_rms
-python mlp-main.py --total-steps 1000000 --env InvertedPendulum-v1 --rms > ./results/refactor/InvertedPendulum-constant-rms
+mkdir -p logs
+CUDA_VISIBLE_DEVICES=1 python atari-main.py  \
+    --cuda \
+    --env BreakoutNoFrameskip-v4 \
+    --num-workers 8 \
+    --steps-per-batch 256

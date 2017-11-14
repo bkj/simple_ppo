911746582882de845c91d3d4f9d9679a65952ac0
bjohnson <bkj.322@gmail.com>
2017-11-14 01:22:49 -0500

    ...

---
+++ plot.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np
+import pandas as pd
+from glob import glob
+
+from rsub import *
+from matplotlib import pyplot as plt
+
+def smooth_reward_curve(x, y):
+    k      = min(31, int(np.ceil(len(x) / 30)))
+    xsmoo = x[k:-k]
+    ysmoo = np.convolve(y, np.ones(2 * k + 1), mode='valid') / np.convolve(np.ones_like(y), np.ones(2 * k + 1), mode='valid')
+    
+    downsample = max(int(np.floor(len(xsmoo) / 1e3)), 1)
+    return xsmoo[::downsample], ysmoo[::downsample]
+
+
+def load_data(indir='.'):
+    infiles = glob(os.path.join(indir, '*.monitor.csv'))
+    df = pd.concat([pd.read_csv(f, skiprows=1) for f in infiles], axis=0)
+    df = df.sort_values('t').reset_index(drop=True)
+    df['timesteps'] = df.l.cumsum()
+    df['timesteps'] = np.concatenate([[0], df.timesteps])[:-1]
+    
+    return smooth_reward_curve(
+        np.array(df.timesteps),
+        np.array(df.r),
+    )
+
+
+x, y = load_data('./logs')
+
+_ = plt.plot(x, y)
+show_plot()
